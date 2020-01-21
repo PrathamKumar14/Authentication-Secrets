8c3e66d6b0e1729f3828d648777c91ee1f52d705
Pratham kumar <prathamsolera@gmail.com>
2020-01-21 12:38:22 +0530

    Level 5 authentication with passport.js

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
-// const bcrypt = require("bcrypt");
-// const saltRounds = 10;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+
 const app = express();
 
 app.use(express.static("public"));
@@ -19,14 +20,31 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+app.use(session({
+  secret: "Our little secrets.",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
 
+userSchema.plugin(passportLocalMongoose);
+
 const User = mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res){
   res.render("home");
@@ -40,72 +58,49 @@ app.get("/register", function(req, res){
   res.render("register");
 });
 
+app.get("/secrets", function(req, res){
+
+  if(req.isAuthenticated()){
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.get("/logout", function(req, res){
+  req.logout();
+  res.redirect("/");
+});
+
 app.post("/register", function(req, res){
 
-   /////////////////////////////////// For level 4 salting and hashing bcrypt ////////////////////////////////////////////
-
-   // bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-   //    newUser = new User({
-   //      email: req.body.username,
-   //      password: hash
-   //    });
-   //
-   //    newUser.save(function(err){
-   //      if(err){
-   //        console.log(err);
-   //      } else {
-   //          res.render("secrets");
-   //      }
-   //    });
-   // });
-
-  newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)
-  });
-
-  newUser.save(function(err){
+  User.register({username: req.body.username}, req.body.password, function(err, user){
     if(err){
       console.log(err);
+      res.redirect("/register");
     } else {
-        res.render("secrets");
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/secrets");
+      });
     }
   });
+
 });
 
 app.post("/login", function(req, res){
 
-  /////////////////////////////////// For level 4 salting and hashing bcrypt ////////////////////////////////////////////
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
 
-  // const username = req.body.username;
-  // const password = req.body.password;
-  //
-  // User.findOne({email: username}, function(err, foundUser){
-  //   if(err){
-  //     console.log(err);
-  //   } else {
-  //     if(foundUser){
-  //       bcrypt.compare(password, foundUser.password, function(err, result) {
-  //         if(result === true){
-  //           res.render("secrets");
-  //         }
-  //       });
-  //     }
-  //   }
-  // });
-
-  const username = req.body.username;
-  const password = md5(req.body.password);
-
-  User.findOne({email: username}, function(err, foundUser){
+  req.login(user, function(err){
     if(err){
       console.log(err);
     } else {
-      if(foundUser){
-        if(foundUser.password === password){
-          res.render("secrets");
-        }
-      }
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/secrets");
+      });
     }
   });
 
