71f5e27ce2c0c4d80c78072089c6da2fb976a77c
Pratham kumar <prathamsolera@gmail.com>
2020-01-21 11:14:12 +0530

    level 4 authentication salting and hashing with bcrypt

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const md5 = require("md5");
+// const bcrypt = require("bcrypt");
+// const saltRounds = 10;
 const app = express();
 
 app.use(express.static("public"));
@@ -39,6 +41,24 @@ app.get("/register", function(req, res){
 });
 
 app.post("/register", function(req, res){
+
+   /////////////////////////////////// For level 4 salting and hashing bcrypt ////////////////////////////////////////////
+
+   // bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+   //    newUser = new User({
+   //      email: req.body.username,
+   //      password: hash
+   //    });
+   //
+   //    newUser.save(function(err){
+   //      if(err){
+   //        console.log(err);
+   //      } else {
+   //          res.render("secrets");
+   //      }
+   //    });
+   // });
+
   newUser = new User({
     email: req.body.username,
     password: md5(req.body.password)
@@ -54,6 +74,25 @@ app.post("/register", function(req, res){
 });
 
 app.post("/login", function(req, res){
+
+  /////////////////////////////////// For level 4 salting and hashing bcrypt ////////////////////////////////////////////
+
+  // const username = req.body.username;
+  // const password = req.body.password;
+  //
+  // User.findOne({email: username}, function(err, foundUser){
+  //   if(err){
+  //     console.log(err);
+  //   } else {
+  //     if(foundUser){
+  //       bcrypt.compare(password, foundUser.password, function(err, result) {
+  //         if(result === true){
+  //           res.render("secrets");
+  //         }
+  //       });
+  //     }
+  //   }
+  // });
 
   const username = req.body.username;
   const password = md5(req.body.password);
